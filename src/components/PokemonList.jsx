fa35f1889ad911bfacaa8e59bdd5e7b52077520d
Soumik Majumder <soumikcode02@gmail.com>
2024-07-18 22:19:40 +0530

    simplified usestate variables, storing them as one single object

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -5,24 +5,20 @@ import Pokemon from "./Pokemon";
 
 function PokemonList()
 {
-    const [pokemonList, setPokemonList] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/');
-    const[nextUrl, setNextUrl] = useState("");
-    const[prevUrl, setPrevUrl] = useState("");
+    const [pokeObj, setPokeObj] = useState({
+        pokemonList : [],
+        isLoading : true,
+        url : 'https://pokeapi.co/api/v2/pokemon/',
+        nextUrl : "",
+        prevUrl : ""
+    })
 
     async function downloadPokemons()
     {
-        setIsLoading(true);
-        const response = await axios.get(url);
+        setPokeObj((state) => ({...state, isLoading : true}));
+        const response = await axios.get(pokeObj.url);
 
-        setNextUrl(response.data.next);
-        setPrevUrl(response.data.previous);
-
-        // console.log(prevUrl);
-        // console.log(nextUrl);
-        
-        // console.log(response.data.results);
+        setPokeObj((state) => ({...state, nextUrl : response.data.next, prevUrl : response.data.previous}));
 
         const pokemonResults = response.data.results;
 
@@ -39,33 +35,36 @@ function PokemonList()
             }
         });
 
-        // console.log(myResult);
-        // console.log(response.data);
-
-        // setPokemonList(myResult);
-        setPokemonList([...myResult]);
-        setIsLoading(false);
+        setPokeObj((state) => ({...state, pokemonList : [...myResult], isLoading : false}));
     }
 
     useEffect(() => {
         downloadPokemons();
-    }, [url])
+    }, [pokeObj.url])
 
     const buttonClass = (isDisabled) => `rounded-[5px] p-[5px_0px] w-[75px] ${isDisabled? "bg-[#d4d4d4]" : "bg-[#f94f71] hover:bg-[crimson]"} duration-[0.1s] text-white font-bold`;
 
     return (
         <div className="m-[10px_auto] flex gap-[10px] flex-wrap flex-col items-center justify-center">
             <div className="font-bold">Pokemon list</div>
-            <div className={`grid ${isLoading? "grid-cols-1" : "grid-cols-4"} ${isLoading? "md:grid-cols-1" : "md:grid-cols-5"} gap-[10px] md:gap-[100px]`}>
+            <div className="flex justify-between w-[300px] md:w-[500px] items-center mt-[15px] mb-[15px]">
+                <button disabled={pokeObj.prevUrl == null} className={buttonClass(pokeObj.prevUrl == null)} onClick={() => setPokeObj((state) => ({...state, url : pokeObj.prevUrl}))} >Prev</button>
+                <button disabled={pokeObj.nextUrl == null} className={buttonClass(pokeObj.nextUrl == null)} onClick={() => setPokeObj((state) => ({...state, url : pokeObj.nextUrl}))}>Next</button>
+            </div>
+            <div className={`grid ${pokeObj.isLoading? "grid-cols-1" : "grid-cols-4"} ${pokeObj.isLoading? "md:grid-cols-1" : "md:grid-cols-5"} gap-[10px] md:gap-[100px]`}>
             {/* <div className={`grid grid-cols-${isLoading? "1" : "4"} md:grid-cols-${isLoading? "1" : "5"} gap-[10px] md:gap-[30px] lg:gap-[60px]`}> */}
             {
-                isLoading? <div className="font-bold">Loading...</div> :
-                pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} id={p.id}/>)
+                pokeObj.isLoading? <div className="font-bold">Loading...</div> :
+                pokeObj.pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} id={p.id}/>)
             }
             </div>
             <div className="flex justify-between w-[300px] md:w-[500px] items-center mt-[15px]">
-                <button disabled={prevUrl == null} className={buttonClass(prevUrl == null)} onClick={() => setUrl(prevUrl)} >Prev</button>
-                <button disabled={nextUrl == null} className={buttonClass(nextUrl == null)} onClick={() => setUrl(nextUrl)}>Next</button>
+                {
+                    pokeObj.isLoading? "" : <>
+                        <button disabled={pokeObj.prevUrl == null} className={buttonClass(pokeObj.prevUrl == null)} onClick={() => setPokeObj((state) => ({...state, url : pokeObj.prevUrl}))} >Prev</button>
+                        <button disabled={pokeObj.nextUrl == null} className={buttonClass(pokeObj.nextUrl == null)} onClick={() => setPokeObj((state) => ({...state, url : pokeObj.nextUrl}))}>Next</button>
+                    </>
+                }
             </div>
         </div>
     )
