e4bd44b818b3ba84fcaec59dd77b0401444601da
Soumik Majumder <soumikcode02@gmail.com>
2024-07-21 11:15:50 +0530

    simplified logic for pokemon search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,24 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 
-import useDebounce from "../../hooks/useDebounce"
+import { useRef } from "react"
 
 function Search({ setSearchTerm })
 {
-    const debouncedCallback = useDebounce((e) => setSearchTerm(e.target.value.toLowerCase()));
+    const timerId = useRef(null);
+
+    function debounce(e)
+    {
+        clearTimeout(timerId.current);
+        timerId.current = setTimeout(() => {
+            setSearchTerm(e.target.value.toLowerCase());
+        }, 1500)
+    }
 
     return (
         <div>
             <input className="w-[300px] md:w-[500px] border-[1px] border-black p-[10px] rounded-[4px] outline-none mb-[5px] mt-[10px]" type="text" placeholder="pokemon name"
-            onChange={debouncedCallback}
+            onChange={debounce}
             />
         </div>
     )
